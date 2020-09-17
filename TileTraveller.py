c5ea6ae9d2de8b462f6172287aedc9610de5b28f
Gisli Steinn Gislason <gislisg18@ru.is>
2020-09-17 09:35:11 +0000

    Algorythm written

---
+++ TileTraveller.py
@@ -0,0 +1,9 @@
+#We start by making 2 variables one that keeps track of how high the user is and another that keeps track of how much to the sides he goes
+#Lets call them H and W and start them both at 1
+#We use a while loop so the game continues to run until he hits tile W3 and H1
+#At any point if his W is 1 he can only travel east, if its 3 he can only travel west and if it is 2 he can travel both ways
+#If his H is 1 he can only travel north and if it is 3 he can only travel south, if it is 2 he can travel both ways
+#Make sure the users input always get interpreted as a lowercase letter
+#Lets make a function to control his movements
+#We make restrictions for the middle boxes and the directions he cannot go
+#We end the game when he hits W3 H1
